455998d630089dab4ffde0d8affc7811c4b956e2
scg88 <49008790+scg88@users.noreply.github.com>
2019-05-06 22:18:26 -0700

    Update Locator.java

--- Locator.java
+++ Locator.java
@@ -1,20 +1,17 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+/* Significant Contributions by:
+ * Spencer Green
+ * Aaron Inocelias
+ */
+
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.TreeMap;
-
 
 public class Locator {
 	//private variables
 	private static Scanner input = new Scanner(System.in);
-	private static Scanner input2 = new Scanner(System.in);
 	private static TreeMap<String, Integer> tableID = new TreeMap<String, Integer>();
 	private static TreeMap<String, String> tableLocation = new TreeMap<String, String>();
-	private static ArrayList<String> usernameList = new ArrayList<>();
 	
 	public static void main(String [] args) throws IOException {
 		String username = "";
@@ -24,40 +21,37 @@ public class Locator {
 		getUsername(username, location, ID);
 	}
 
-	
-	//Anywhere code could be made more Robust in class methods?
-		public static void initializeLocater() throws IOException {
-			Application.makeFileID();
-			Application.makeFileLocation();
-			tableID = Application.getTableID();
-			tableLocation = Application.getTableLocation();
-			usernameList = Application.getUsernameList();
-		}
+	public static void initializeLocater() throws IOException {
+		Application.makeFileID();
+		Application.makeFileLocation();
+		tableID = Application.getTableID();
+		tableLocation = Application.getTableLocation();
+	}
 		
-		public static void getUsername(String username, String location, int ID) throws IOException {
-			System.out.println("Enter the user you wish to update: ");
-			username = input.nextLine();
-			if(!(tableID.containsKey(username))) {
-				System.out.println("That user does not exist: ");
-				getUsername(username, location, ID);
-			}else {
-				getID(username, location, ID);
-			}
+	public static void getUsername(String username, String location, int ID) throws IOException {
+		System.out.print("Enter the username associated with the device: ");
+		username = input.nextLine();
+		if(!(tableID.containsKey(username))) {
+			System.out.println("That user does not exist: ");
+			getUsername(username, location, ID);
+		}else{
+			getID(username, location, ID);
 		}
+	}
 		
-		public static void getID(String username, String location, int ID) throws IOException {
-			System.out.println("Enter the ID associated with " + username +": ");
-			ID = input.nextInt();
-			if(!(tableID.get(username) == ID)) {
-				System.out.println("That ID does not exist, or is not associated with that user: ");
-				getID(username, location, ID);
-			}else {
-				getLocation(username, location, ID);
-			}
+	public static void getID(String username, String location, int ID) throws IOException {
+		System.out.print("Enter the ID associated with " + username +"'s device: ");
+		ID = input.nextInt();
+		if(!(tableID.get(username) == ID)) {
+			System.out.println("That ID does not exist, or is not associated with that user: ");
+			getID(username, location, ID);
+		}else{
+			getLocation(username, location, ID);
 		}
+	}
 		
-		public static void getLocation(String username, String location, int ID) throws IOException {
-			System.out.println("The location of " + username + " is " + tableLocation.get(username)+ ".");
-		}
+	public static void getLocation(String username, String location, int ID) throws IOException {
+		System.out.println("The location of " + username + "'s device is in " + tableLocation.get(username)+ ".");
+	}
 
 }
